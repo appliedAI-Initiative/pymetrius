37b33ff6d52670861c9c56328bacec0d21be1ffb
Michael Panchenko <michael.panchenko@unternehmertum.de>
2020-10-12 14:50:34 +0200

    Minor improvements in requirements, versioning and project set-up

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,20 +14,26 @@ if "{{cookiecutter.include_readthedocs_yaml}}" != "y":
 
 
 return_code = os.system("""
-git init
 echo "Initializing your new project in in $(pwd). This might take a while"
+git init
+git remote origin https://gitlab.aai.lab/{{cookiecutter.gitlab_project_path}}
 echo "Creating and activating venv"
-python -m venv ./venv
+python{{cookiecutter.python_version}} -m venv ./venv
 . venv/bin/activate
-echo "Installing {{cookiecutter.project_name}} in editable mode into venv"
-pip install -e .
-echo "Installing development dependencies"
-pip install -r requirements-dev.txt
+echo "Installing formatter"
+pip install black pre-commit
 echo "Performing Initial formatting"
 black .
+echo "Setting git hooks"
+pre-commit install
+pre-commit autoupdate
 echo "Initial commit"
 git add . && git commit -m "Initial commit by python_library_template"
 git branch develop && git checkout develop
+echo "Installing {{cookiecutter.project_name}} in editable mode into venv"
+pip install -e .
 echo "Running the sample script"
 python scripts/run_sample.py
+echo "A virtual environment for your project has been created in $(pwd)/venv.\
+ The library was installed there in editable mode."
 """)
