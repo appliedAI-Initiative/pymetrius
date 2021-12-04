9f2206b92a1a6f404d3a7691da36ee6a0d16c238
Michael Panchenko <michael.panchenko@unternehmertum.de>
2021-12-04 23:45:02 +0100

    Minor fix in update_docs.py

--- {{cookiecutter.project_name}}/build_scripts/update_docs.py
+++ {{cookiecutter.project_name}}/build_scripts/update_docs.py
@@ -88,7 +88,11 @@ def make_rst(src_root="src", docs_root="docs", clean=False, overwrite=False):
     for top_level_package_name in os.listdir(src_root):
         top_level_package_dir = os.path.join(src_root, top_level_package_name)
         # skipping things in src that are not packages, like .egg files
-        if not os.path.isdir(top_level_package_dir) or "." in top_level_package_name:
+        if (
+            not os.path.isdir(top_level_package_dir)
+            or "." in top_level_package_name
+            or top_level_package_name.startswith("_")
+        ):
             continue
 
         log.info(f"Generating docu for top-level package {top_level_package_name}")
